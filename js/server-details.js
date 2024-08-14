1a1c6bb23290e67359609f175ada918113b29419
Hp store desktop <aagortey@gmail.com>
2024-08-14 21:09:07 +0000

    ./js/server-details.js fix

--- js/server-details.js
+++ js/server-details.js
@@ -2,5 +2,8 @@
     const isProduction = false; // Set to true for production, false for development
 
     // Set serverName and portNumber based on the environment
-    const serverName = isProduction ? 'https://crypto-invest-server.onrender.com' : 'http://localhost';
-    const portNumber = isProduction ? '' : 3000; // Leave portNumber empty for production
+    // const serverName = isProduction ? 'https://crypto-invest-server.onrender.com' : 'http://localhost';
+    // const portNumber = isProduction ? '' : 3000; // Leave portNumber empty for production
+    const portNumber = 3000;
+    const baseUrl = 'https://crypto-invest-server.onrender.com';
+    const localBaseUrl = `http://localhost:${3000}`;
